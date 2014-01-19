5aa452e6878798ced51e4cfb232faa24e68ff7db
Zach Latta <zach@zachlatta.com>
2014-01-18 23:09:52 -0800

    Form upload works.

--- packrat/packrat.go
+++ packrat/packrat.go
@@ -2,8 +2,18 @@ package packrat
 
 import (
 	"html/template"
+	"io"
 	"net/http"
+
+	"appengine"
+	"appengine/blobstore"
 )
+
+type Game struct {
+	Name  string
+	Email string
+	File  []byte
+}
 
 var templates = template.Must(template.ParseFiles(
 	"packrat/application.html",
@@ -15,10 +25,45 @@ func renderTemplate(w http.ResponseWriter, tmpl string, i interface{}) {
 	}
 }
 
+func serveError(c appengine.Context, w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "text/plain")
+	io.WriteString(w, "Internal Server Error")
+	c.Errorf("%v", err)
+}
+
 func init() {
 	http.HandleFunc("/", root)
+	http.HandleFunc("/new", submitGame)
+	http.HandleFunc("/serve/", handleServe)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "index", nil)
+	c := appengine.NewContext(r)
+	uploadURL, err := blobstore.UploadURL(c, "/new", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "text/html")
+	renderTemplate(w, "index", uploadURL)
+}
+
+func submitGame(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	blobs, _, err := blobstore.ParseUpload(r)
+	if err != nil {
+		serveError(c, w, err)
+		return
+	}
+	file := blobs["file"]
+	if len(file) == 0 {
+		c.Errorf("No file uploaded.")
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	http.Redirect(w, r, "/serve/?blobKey="+string(file[0].BlobKey), http.StatusFound)
+}
+
+func handleServe(w http.ResponseWriter, r *http.Request) {
+	blobstore.Send(w, appengine.BlobKey(r.FormValue("blobKey")))
 }
